9596bff08123d9268a48478f9737405f85c8dc2a
Keshav <kumarkeshav825@gmail.com>
2021-06-13 13:32:01 -0400

    Revert "Search working"
    This reverts commit a4f0454d1f4fea92627430e29d03a85f85f10ef3.

--- JS/app.js
+++ JS/app.js
@@ -1,61 +1,61 @@
 // console.log('OM NAMAY SIVAY');
 showNote();
 
-let addBTn = document.getElementById('addBtn');
+let addBTn=document.getElementById('addBtn');
 
 
-addBTn.addEventListener('click', function (e) {
-    console.log('working');
-    let addTxt = document.getElementById('addTxt');
-    let notes = localStorage.getItem("notes");
-    if (notes == null) {
-        notesObj = [];
+addBTn.addEventListener('click',function(e){
+   console.log('working');
+   let addTxt=document.getElementById('addTxt');
+    let notes =localStorage.getItem("notes");
+    if (notes==null) {
+        notesObj=[]; 
     }
     else {
-        notesObj = JSON.parse(notes);
-    }
-    notesObj.push(addTxt.value);
-    localStorage.setItem("notes", JSON.stringify(notesObj));
+        notesObj=JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes",JSON.stringify(notesObj));
 
-    addTxt.value = "";
-    console.log(notesObj);
-    showNote();
-})
+  addTxt.value="";
+  console.log(notesObj);
+  showNote();
+}) 
 
 
 function showNote() {
-    let html = "";
-    let notes = localStorage.getItem("notes");
+    let html="";
+    let notes =localStorage.getItem("notes");
 
-    if (notes == null) {
-        notesObj = [];
+    if (notes==null) {
+        notesObj=[]; 
     }
     else {
-        notesObj = JSON.parse(notes);
-    }
-    notesObj.forEach(function (element, index) {
+        notesObj=JSON.parse(notes);
+  }
+  notesObj.forEach(function(element,index){
 
 
-        html += `
+html +=`
     <div class="notesCard card my-3 mx-3" style="width: 18rem;">
            
     <div class="card-body">
-      <h5 class="card-title">Note ${index + 1}</h5>
+      <h5 class="card-title">Note ${index +1}</h5>
       <p class="card-text"> ${element}</p>
       <button id="${index}" onclick="deleteNotes(this.id)" class="btn btn-primary">Delete Note</button>
     </div>
-  </div> `
+  </div> ` 
 
 
 
-    })
-    let elem = document.getElementById('notes');
-    if (notesObj.length != 0) {
-        elem.innerHTML = html;
-    }
-    else {
-        elem.innerHTML = 'Nothing to show here';
-    }
+})
+let elem =document.getElementById('notes');
+if (notesObj.length !=0) {
+     elem.innerHTML=html;
+ }
+ else{
+      elem.innerHTML='Nothing to show here';
+}
 
 }
 // function to delete
@@ -65,31 +65,22 @@ function showNote() {
 
 
 function deleteNotes(index) {
-
+ 
     let notes = localStorage.getItem("notes");
 
-    if (notes == null) {
-        notesObj = [];
+     if (notes==null) {
+        notesObj=[]; 
     }
     else {
-        notesObj = JSON.parse(notes);
-    }
+        notesObj=JSON.parse(notes);
+  }
 
-    // console.log("I am deleting " + index);
-    notesObj.splice(index, 1);
-    localStorage.setItem("notes", JSON.stringify(notesObj));
-    showNote();
-
-}
-// Search Function
-let search = document.getElementById('searchTxt');
-
-
-search.addEventListener('input', function () {
+  console.log("I am deleting " +index);
+  notesObj.splice(index,1);
+localStorage.setItem("notes",JSON.stringify(notesObj));
+showNote();
     
-    let inputVal=search.value.toLowerCase();
-    // console.log("This is working " +inputVal);
-    
+<<<<<<< HEAD
     let noteCard=document.getElementsByClassName('notesCard');
     Array.from(noteCard).forEach(function(element){
         let carTxt=element.getElementsByTagName('p')[0].innerText;
@@ -116,3 +107,6 @@ search.addEventListener('input', function () {
 
 
 
+=======
+}
+>>>>>>> parent of a4f0454... Search working
