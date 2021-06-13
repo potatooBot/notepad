be102aa951ea0a5b5850f2c15b84d2e078ca522f
Keshav <kumarkeshav825@gmail.com>
2021-06-13 10:04:22 -0400

    "LOCAL STORAGE ADDED"

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,19 @@
 console.log('OM NAMAY SIVAY')
 let addBTn=document.getElementById('addBtn');
+
 addBTn.addEventListener('click',function(e){
-   console.log('working'); 
+   console.log('working');
+   let addTxt=document.getElementById('addTxt');
+    let notes =localStorage.getItem("notes");
+    if (notes==null) {
+        notesObj=[]; 
+    }
+    else {
+        notesObj=JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes",JSON.stringify(notesObj));
+
+  addTxt.value="";
+  console.log(notesObj);
 })
